a554f057e5c547be5d8f7bb30afc5004a2ed4535
Rahul Soni <rahul.soni3883@gmail.com>
2017-01-19 19:45:51 +0530

    Created Sidebar for Pricing Widget

---
+++ sidebar-pricing.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The sidebar containing the Widget Pricing.
+ *
+ * @package Blank Theme
+ */
+?>
+
+<div id="st-widget-pricing-wrapper" class="st-widget-pricing-container row">
+  <?php dynamic_sidebar( 'sidebar-widget-pricing' ); ?>
+</div><!-- #widget-pricing -->
