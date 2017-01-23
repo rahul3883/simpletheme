0cbb5e16625bd0c31010fbcbff7ef8dbba267e45
Rahul Soni <rahul.soni3883@gmail.com>
2017-01-23 20:33:56 +0530

    Added html elements for proper styling

--- template-parts/content-team.php
+++ template-parts/content-team.php
@@ -14,7 +14,7 @@
 		$st_team_obj = get_post_type_object( 'st_team' );
 		?>
 
-		<h2 class="st-page-title"><?php echo $st_team_obj->labels->name; ?></h2>
+		<p class="st-page-title"><?php echo $st_team_obj->labels->name; ?></p>
 	</header>
 
 	<div class="st-page-content row">
@@ -23,6 +23,7 @@
 		$args = array(
 			'post_type'				=> 'st_team',
 			'posts_per_page'	=> 4,
+			'order'						=> 'ASC',
 		);
 
 		$team_members = new WP_Query( $args );
@@ -32,13 +33,13 @@
 				$team_members->the_post();
 				?>
 
-				<div style="text-align:center;" class="large-3 column">
-          <div class="st-team-member-thumbnail">
-            <?php the_post_thumbnail( 'st-team' ); ?>
-          </div>
-					<h3 class="st-team-member-title"><?php the_title(); ?></h3>
-          <h4 class="st-team-member-designation"><?php echo get_post_meta( get_the_ID(), '_team_member_designation', true ) ?></h4>
-          <p class="st-team-member-excerpt"><?php echo wp_kses_post( wp_trim_words( get_the_excerpt(), 15 ) ); ?></p>
+				<div class="large-3 column">
+					<div class="st-team-member-thumbnail">
+						<?php the_post_thumbnail( 'st-team' ); ?>
+					</div>
+					<p class="st-team-member-title st-team-text"><?php the_title(); ?></p>
+					<p class="st-team-member-designation st-team-text"><?php echo get_post_meta( get_the_ID(), '_team_member_designation', true ) ?></p>
+					<p class="st-team-member-excerpt st-team-text"><?php echo wp_kses_post( wp_trim_words( get_the_excerpt(), 15 ) ); ?></p>
 				</div>
 
 			<?php } ?>
